3f8b0b0cf4ea7757765014a9904707f48004b092
Luis M <77978673+OrangeXR@users.noreply.github.com>
2026-02-13 11:40:01 -0600

    Refactor Flask app and improve routing
    Updated the Flask application structure and routes.

--- Main.py
+++ Main.py
@@ -195,6 +195,9 @@ def delete_item(item_id, player_id):
     return redirect(url_for("inventory_page", player_id=player_id))
 
 
+# Change from Trial 2
+
+
 # ======================
 # Uncomment this section
 # to serve over network
@@ -213,3 +216,4 @@ def delete_item(item_id, player_id):
 if __name__ == "__main__":
 
     app.run(debug=True)
+

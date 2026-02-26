f9792dcc357e4ed3c045d26c596d0bbd4e35febc
Luis M <77978673+OrangeXR@users.noreply.github.com>
2026-02-25 20:04:56 -0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -13,13 +13,15 @@ app = Flask(__name__)
 app.secret_key = "super-secret-key"
 
 BASE_DIR = app.root_path
-load_dotenv("/etc/secrets/.env")  # Load .env file
+#load_dotenv()  # Load .env file
 
-#OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY") # ============== for render 
+
+
+OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY") # ============== for render 
 
 # OPENROUTER_API_KEY=""
 #print("DEBUG — OPENROUTER_API_KEY =", repr(OPENROUTER_API_KEY)) # Makes sure the correct key is being sent out
-
+print("Render sees:", repr(os.environ.get("OPENROUTER_API_KEY")))
 
 
 
@@ -507,6 +509,7 @@ if __name__ == "__main__":
 # for render.com
 # gunicorn app:app
 # ================
+
 
 
 

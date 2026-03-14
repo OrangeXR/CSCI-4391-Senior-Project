503ff5c33272eee65e29223fbdcb2645b9361db4
Luis M <77978673+OrangeXR@users.noreply.github.com>
2026-03-13 20:17:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 # Connect to db
 # =============
 def get_db():
-    conn = sqlite3.connect("src/instance/inventory.db")
+    conn = sqlite3.connect("instance/inventory.db")
     conn.row_factory = sqlite3.Row
     return conn
 

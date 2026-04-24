d658f4eb9547c63ae2d529f6ee972080dde55a71
Luis M <77978673+OrangeXR@users.noreply.github.com>
2026-04-24 13:10:10 -0500

    Update database.py

--- database.py
+++ database.py
@@ -5,4 +5,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "instance", "inventory.db")
 
 def get_db():
-    return sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row 
+    return conn

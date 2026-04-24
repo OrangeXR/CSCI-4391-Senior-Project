2233aa6a2498222b1c8c3e8a6b1564fdc4eab23b
Luis M <77978673+OrangeXR@users.noreply.github.com>
2026-04-24 12:59:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -306,7 +306,7 @@ def logout():
 @app.route("/inventory",methods=["GET", "POST"])
 @login_required
 def inventory_page():
-    from src.openrouterllm import Ai_Chat   #importing everything from openrouterllm
+    from openrouterllm import Ai_Chat   #importing everything from openrouterllm
     from validator import recipe_validator #importing everything from openrouterllm
     player_id = session["player_id"]
 
